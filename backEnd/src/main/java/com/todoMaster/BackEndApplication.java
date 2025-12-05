560bd0534b48f7cdaa2178bed479e6f4735183a5
vivi372 <ym970825@naver.com>
2025-12-05 14:23:21 +0900

    파일 / 폴더 구조 수정

--- backEnd/src/main/java/com/todoMaster/BackEndApplication.java
+++ backEnd/src/main/java/com/todoMaster/BackEndApplication.java
@@ -1,9 +1,11 @@
-package com.todoMarster;
+package com.todoMaster;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.todoMaster.**.mapper")
 public class BackEndApplication {
 
 	public static void main(String[] args) {
