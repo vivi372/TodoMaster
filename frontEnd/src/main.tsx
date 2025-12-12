2e405de833f6cbcf44fa9ad54923b1300a2bd324
vivi372 <ym970825@naver.com>
2025-12-12 23:30:34 +0900

    frontEnd) sentry / RHF,zod 추가

--- frontEnd/src/main.tsx
+++ frontEnd/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
+import * as Sentry from '@sentry/react';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+// PRODUCTION 환경 + DSN 존재할 때만 초기화
+if (import.meta.env.PROD && import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+    tracesSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    environment: 'production',
+  });
+}
+
+createRoot(document.getElementById('root')!).render(<App />);
--- frontEnd/vite.config.ts
+++ frontEnd/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,19 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    // prod에서만 Sentry sourcemap 업로드
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          sentryVitePlugin({
+            org: 'venivivi',
+            project: 'javascript-react',
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+            silent: false,
+          }),
+        ]
+      : []),
   ],
-})
+  build: {
+    sourcemap: true, // Sentry에 필요
+  },
+});
